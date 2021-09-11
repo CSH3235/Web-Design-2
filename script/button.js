4d3fff6dc3fb41eaea56dd0d53336b2fe2adcdce
CSH <csh.dev05@gmail.com>
2021-09-11 23:18:38 +0900

    fixed files

--- script/button.js
+++ script/button.js
@@ -1,22 +1,23 @@
 window.onload=function() {
     let sp1 = document.getElementById("SP1");
     let sp2 = document.getElementById("SP2");
-    
+    let sp3 = document.getElementById("SP3");
+    sp1.classList.remove("square_part_1_container");
+
     sp2.classList.remove("square_part_2_container_re");
     sp2.classList.remove("square_part_2_container");
     
+
+    sp3.classList.remove("square_part_3_container_re");
+    sp3.classList.remove("square_part_3_container");
     let NextBtn = document.getElementById("nextBtn");
     
-    sp1.classList.remove("square_part_1_container");
 
     let ClickCount = 0;
 
     NextBtn.addEventListener("click", function(e){
         if (ClickCount === 0) {
             e.preventDefault;
-
-            sp1.classList.remove("square_part_1_container");
-
             void sp1.offsetWidth;
 
             sp1.classList.add("square_part_1_container");
@@ -24,8 +25,6 @@ window.onload=function() {
 
             setTimeout(function(){
                 e.preventDefault;
-                sp2.classList.remove("square_part_2_container");
-                sp2.classList.remove("square_part_2_container_re");
                 void sp2.offsetWidth;
                 sp2.classList.add("square_part_2_container_re");
             }, 1000)
@@ -34,13 +33,33 @@ window.onload=function() {
         else if (ClickCount === 1) {
             e.preventDefault;
             sp2.classList.remove("square_part_2_container_re");
-
             sp2.classList.remove("square_part_2_container");
-
             void sp2.offsetWidth;
             sp2.classList.add("square_part_2_container");
+            ClickCount += 1;
+            setTimeout(function(){
+                e.preventDefault;
+                void sp3.offsetWidth;
+                sp3.classList.add("square_part_3_container_re");
+            }, 1000)
         }
 
+        else if (ClickCount === 2) {
+            e.preventDefault;
+            sp3.classList.remove("square_part_3_container_re");
+            sp3.classList.remove("square_part_3_container");
+            void sp3.offsetWidth;
+            sp3.classList.add("square_part_3_container");
+            sp1.classList.remove("square_part_1_container");
+        
+            setTimeout(function(){
+                e.preventDefault;
+                sp1.classList.remove("square_part_1_container_re");
+                void sp1.offsetWidth;
+                sp1.classList.add("square_part_1_container_re");
+            }, 1000)
+        
+        }
     }, false);
 }
 
