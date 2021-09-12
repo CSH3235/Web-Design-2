779501ac04b11181364ae604a290686eae6bd6f3
CSH <csh.dev05@gmail.com>
2021-09-12 19:55:56 +0900

    rotate

--- script/button.js
+++ script/button.js
@@ -22,22 +22,33 @@ window.onload=function() {
 
     NextBtn.addEventListener("click", function(e){
         if (ClickCount === 0) {
+            
             e.preventDefault;
             void sp1.offsetWidth;
-
+            
             sp1.classList.remove("square_part_1_container_re");
             sp1.classList.add("square_part_1_container");
-            ClickCount += 1;
 
+            sp2.classList.remove("square_part_2_container_re");
+            sp2.classList.remove("square_part_2_container");
+            
+
+            sp3.classList.remove("square_part_3_container");
+            sp3.classList.remove("square_part_3_container_re");
+            ClickCount += 1;
             setTimeout(function(){
+                for(let i = 1; i < node1.length; i += 2) {
+                    node1[i].style.zIndex = 0;
+                }
                 e.preventDefault;
                 void sp2.offsetWidth;
                 sp2.classList.add("square_part_2_container_re");
-            }, 1000)
+            }, 100)
         }
-
+        
         else if (ClickCount === 1) {
             e.preventDefault;
+
             sp2.classList.remove("square_part_2_container_re");
             sp2.classList.remove("square_part_2_container");
             void sp2.offsetWidth;
@@ -47,7 +58,7 @@ window.onload=function() {
                 e.preventDefault;
                 void sp3.offsetWidth;
                 sp3.classList.add("square_part_3_container_re");
-            }, 1000)
+            }, 100)
         }
 
         else if (ClickCount === 2) {
@@ -66,8 +77,8 @@ window.onload=function() {
                 for(let i = 1; i < node1.length; i += 2) {
                     node1[i].style.zIndex = 99;
                 }
-                
-            }, 1000)
+                ClickCount = 0;
+            }, 100)
         
         }
     }, false);
