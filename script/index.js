be0f3def9b96c7a0ea988557595c4e6db7294c93
CSH <csh.dev05@gmail.com>
2021-12-12 23:26:58 +0900

    ani motion added

--- script/index.js
+++ script/index.js
@@ -1,10 +1,7 @@
-// let sp1 = document.getElementById("SP1");
-
-// let node1 = sp1.childNodes;
-
-
-// for(let i = 1; i < node1.length; i += 2) {
-//     console.log(i)
-//     console.log(node1[i])
-//     node1[i].style.zIndex = 99;
-// }
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById('ani'),
+    rederer: 'svg',
+    loop:true,
+    autoplay:true,
+    path: '../data.json'
+})
