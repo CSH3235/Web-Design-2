24223a92814a9800b34f0a660d69e47f84e43fa3
CSH <csh.dev05@gmail.com>
2021-12-30 23:40:06 +0900

    add snowfall effect

--- script/index.js
+++
@@ -1,9 +0,0 @@
-let animation = bodymovin.loadAnimation({
-    
-    container: document.getElementById('ani'),
-    rederer: 'svg',
-    loop:true,
-    autoplay:true,
-    path: '../data.json'
-})
-
